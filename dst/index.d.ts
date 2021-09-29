add0e9690cb23af1bc7713528c0687e94b3e0c5d
ChasJC23 <45319866+ChasJC23@users.noreply.github.com>
2021-09-29 01:15:21 +0100

    Delete dst directory

--- dst/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./color";
-export * from "./gradient";
-export * as colorConsole from "./console";
-export * as web from "./web";
-//# sourceMappingURL=index.d.ts.map
--- dst/web.d.ts
+++
@@ -1,8 +0,0 @@
-import { Color } from "./color";
-import { Gradient } from "./gradient";
-export declare function generateStyle(color: Color, isBg?: boolean): string;
-export declare function uniform(element: HTMLElement, color: Color, isBg?: boolean): void;
-export declare function cyclicUniform(element: HTMLElement, segmentLength: number, isBg?: boolean, ...colors: Color[]): void;
-export declare function gradient(element: HTMLElement, gradient: Gradient, isBg?: boolean): void;
-export declare function cyclicGradient(element: HTMLElement, segmentLength: number, isBg?: boolean, ...gradients: Gradient[]): void;
-//# sourceMappingURL=web.d.ts.map
